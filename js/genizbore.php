38b2949dc80f3a458d92621ff4b0fe2cd3835c37
Filip <filip.sostarec@foi.hr>
2013-04-30 03:23:20 +0200

    Podešeno brisanje odgovora.
    Nije se dobro prikazivao gumb za brisanje odgovora pa se to sada
    sredilo.

--- js/genizbore.php
+++ js/genizbore.php
@@ -18,14 +18,14 @@ if($vrj>2){  // glavni IF
 
 			if($tip!="prazno")echo "<div style='float:left;width:25px;margin-right:8px;margin-top:7px;margin-bottom:6px'><input name='odgovor_radio_grupa".$ptbr."' class='prmod' name='prmod' type='".$tip."'/></div>";
 			else echo "<div style='float:left;width:25px;margin-right:8px'>&nbsp;</div>";
-			echo "<div style='float:left;width:350px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_1'/></div>";
+			echo "<div style='float:left;width:300px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_1'/></div>";
 		echo "</div>";
 		echo "<div style='float:left;width:100%;height:6px'>&nbsp;</div>";
 		echo "<div style='float:left:width:100%;margin-top:6px;margin-left:15px;'>";
 			if($tip!="prazno")echo "<div style='float:left;width:25px;margin-right:8px;margin-top:7px;margin-bottom:6px'><input name='odgovor_radio_grupa".$ptbr."' class='prmod' name='prmod' type='".$tip."'/></div>";
 			else echo "<div style='float:left;width:25px;margin-right:8px'>&nbsp;</div>";
 
-			echo "<div style='float:left;width:350px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_2'/></div>";
+			echo "<div style='float:left;width:300px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_2'/></div>";
 		echo "</div>";
 		echo "<div style='float:left;width:100%;height:6px'>&nbsp;</div>";
 
@@ -41,8 +41,8 @@ if($vrj>2){  // glavni IF
 		echo "<div id='kutija_odgovor".$ptbr.$_POST["odg_id"]."' style='float:left:width:100%;margin-top:6px;margin-left:15px;'>";
 			if($tip!="prazno")echo "<div style='float:left;width:25px;margin-right:8px;margin-top:7px;margin-bottom:6px'><input name='odgovor_radio_grupa".$ptbr."' class='prmod' name='prmod' type='".$tip."'/></div>";
 			else echo "<div style='float:left;width:25px;margin-right:8px'>&nbsp;</div>";
-			echo "<div style='float:left;width:350px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_".($_POST["odg_id"]+2)."'/></div>";
-			echo "<div style='float:left;width:40px;margin-left:4px;margin-bottom:6px'><input class='izbrodgovor' pitbr='".$ptbr."' oid='".$_POST["odg_id"]."' style='width:28px;height:28px' type='button' value='X'/></div>";
+			echo "<div style='float:left;width:300px;margin-right:8px;margin-bottom:6px'><input type='text' class='tf' name='odgo_".$ptbr."_".($_POST["odg_id"]+2)."'/></div>";
+			echo "<div style='float:right;width:40px;margin-left:4px;margin-bottom:6px'><input class='izbrodgovor' pitbr='".$ptbr."' oid='".$_POST["odg_id"]."' style='width:28px;height:28px' type='button' value='X'/></div>";
 		
 		echo "<div style='float:left;width:100%;height:6px;'>&nbsp;</div>";
 		echo "</div>";	
